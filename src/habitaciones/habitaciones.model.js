de5a249cfeb1cf88b183d85a520e75950df92286
AndresArtiga333 <andresaartiga057@gmail.com>
2025-04-22 19:15:20 -0600

    feat(habitaciones): se agrego el modelode habitaciones

---
+++ src/habitaciones/habitaciones.model.js
@@ -0,0 +1,39 @@
+import {Schema, model} from 'mongoose';
+
+const habitacionesSchema = new Schema({
+    numero:{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    categoria:{
+        type: String,
+        required: true
+    },
+    precio:{
+        type: String,
+        required: true
+    },
+    reservacion:{
+        type: Boolean,
+        default: false
+    },
+    eventos:[{
+        type: Schema.ObjectId,
+        ref: 'Eventos',
+        default: []
+    }],
+    usuario:{
+        type: Schema.ObjectId,
+        ref: 'User',
+        default: null
+    },
+})
+
+habitacionesSchema.methods.toJSON = function(){
+    const {_id, ...habitaciones} = this.toObject()
+    habitaciones.hid = _id
+    return habitaciones
+}
+
+export default model('Habitaciones', habitacionesSchema)
