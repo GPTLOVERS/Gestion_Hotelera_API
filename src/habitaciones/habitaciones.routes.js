745791ce015083e1aa786f9a0ee1fa63a1cd1429
Zodiacal0 <javierherrera5513@gmail.com>
2025-04-25 13:22:38 -0600

    feat(habitaciones): se agregó el endpoint para reservar

--- src/habitaciones/habitaciones.routes.js
+++ src/habitaciones/habitaciones.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { listarHabitaciones, crearHabitacion, editarHabitacion, eliminarHabitacion } from "./habitaciones.controller.js";
+import { listarHabitaciones, crearHabitacion, editarHabitacion, eliminarHabitacion, reservarHabitacion } from "./habitaciones.controller.js";
 import { listarHabitacionesValidator, crearHabitacionValidator, editarHabitacionValidator,
     eliminarHabitacionValidator
- } from "../middlewares/habitaciones-validator.js";
+} from "../middlewares/habitaciones-validator.js";
 
 const router = Router()
 
 router.get('/listarHabitaciones', listarHabitacionesValidator, listarHabitaciones)
 
 router.post('/crearHabitacion', crearHabitacionValidator, crearHabitacion)
+
+router.patch('/reservaciones/:uid', reservarHabitacion)
 
 router.put('/editarHabitacion/:hid', editarHabitacionValidator, editarHabitacion)
 
